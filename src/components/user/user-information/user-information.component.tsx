144ba72f84b202192362874caaaaf9f1f67d671e
aarancis <alexis2396@hotmail.com>
2025-03-12 19:12:42 +0900

    develop - fix: use mutation to avoid multiple calls with useQuery

--- src/components/user/user-information/user-information.component.tsx
+++ src/components/user/user-information/user-information.component.tsx
@@ -4,22 +4,24 @@ import {getSalesByUser} from '../../../data/rest/sale.service';
 import {useSelector} from 'react-redux';
 import {selectCurrentUser} from '../../../redux/user/user.selector';
 import './user-information.component.scss';
-import {useQuery} from '@tanstack/react-query';
+import {useMutation} from '@tanstack/react-query';
 import {Order} from '../../../domain/interfaces/Order';
 
 const UserInformation = () => {
 
   const user = useSelector(selectCurrentUser);
-  const {data: salesByUser, isSuccess} = useQuery({queryKey: ['salesByUser'], queryFn: () => getSalesByUser(user.id)});
+  const mutation = useMutation<Order[], Error, {id: string}>({
+    mutationFn: () => getSalesByUser(user.id),
+    onSuccess: sales => setSales(sales),
+  });
+
   const [sales, setSales] = useState<Array<Order>>([]);
 
   const orderSales = (): Array<Order> => sales.sort((a: any, b: any) => new Date(a.dateRegister).getTime() > new Date(b.dateRegister).getTime() ? -1 : 1);
 
   useEffect(() => {
-    if (isSuccess) {
-      setSales(salesByUser);
-    }
-  }, [isSuccess, salesByUser]);
+    mutation.mutate({id: user.id});
+  }, []);
 
   return (
     <>
@@ -30,12 +32,12 @@ const UserInformation = () => {
         <hr/>
         <Timeline style={{marginTop: '30px'}} mode="left">
           {
-            isSuccess ? orderSales()
+            sales.length ? orderSales()
               .map((sale: Order) => (
               <Timeline.Item label={new Date(sale.dateRegister).toLocaleString()}>
                 <div className="flex-wrap justify-content-between">
                   <span className="mr-20"><span className="bold">Código: </span> {sale.code}</span>
-                  <span><span className="bold">Precio total: </span>  S/. {sale.salePrice}</span>
+                  <span><span className="bold">Precio total: </span>  {sale.salePrice} 円</span>
                 </div>
                 <hr/>
                 {
@@ -51,7 +53,7 @@ const UserInformation = () => {
                       </div>
                       <div className="flex-column">
                         <span className="bold">Precio del producto </span>
-                        <span className="">S/. {detail.price}</span>
+                        <span className="">{detail.price} 円</span>
                       </div>
                     </div>
                   ))
